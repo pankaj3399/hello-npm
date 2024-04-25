ea96f18a069e23247efd307d1366e598d4ad6dd1
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2024-04-25 08:32:26 +0530

    Package

---
+++ index.js
@@ -0,0 +1,7 @@
+class HelloWorld {
+  sayHello(name) {
+    console.log("Hello" + name);
+  }
+}
+
+module.exports = new HelloWorld();
---
+++ test/script.js
@@ -0,0 +1,2 @@
+const hello = require("hello-npm-pankaj");
+hello.sayHello("pankaj");
